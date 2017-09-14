41215a23cffcec04511c00079ece19ad9ffdacf2
Daniel-Liz <danielliz@gec.inatel.br>
2017-09-14 11:46:43 -0300

    Added printHelloMessage()

--- src/io/github/daniel/Main.java
+++ src/io/github/daniel/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		printHelloMessage();
+	}
 
+	private static void printHelloMessage() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 	}
 
 }
